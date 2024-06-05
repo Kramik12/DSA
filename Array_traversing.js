378fd167b2e07e1bea76580bb3ca28180b100d7d
Kramik <kramikkafle7@gmail.com>
2024-06-05 13:44:47 +0545

    first commit

---
+++ Array_traversing.js
@@ -0,0 +1,11 @@
+// Array traversing
+
+let data = [1, 2, 4, 16, 19, 40];
+for(i=0; i<data.length; i++){
+    console.log(`Array ${i} is ${data[i]} <br>`);
+}
+
+let x = 4;
+console.log(data[x]);
+
+//Array inserting
---
+++ array_inserting.js
@@ -0,0 +1,15 @@
+let data = [0, 5, 7, 10, 6, 11];
+let newEl = 70;
+let pos =2;
+
+for(let i = data.length-1; i>=0; i--){
+    if(i>=pos){
+        data[i+1] = data[i];
+        if(i == pos)
+            {
+                data[i] = newEl;
+            }
+    }
+}
+
+console.log(data);
