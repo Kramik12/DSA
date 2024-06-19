0c4fe4e7c5bfd17d063a2de116f3c288e0ec1259
Kramik <kramikkafle7@gmail.com>
2024-06-19 17:13:39 +0545

    first commit

---
+++ map.js
@@ -0,0 +1,11 @@
+// Map DSA with JavaScript
+
+let data = new Map([
+    ['name', 'Kramik'],
+    [true, 'bool key'],
+    [100, 'hundred']
+]);
+
+data.set('color', 'green');
+console.log(data.size);
+console.log(data);
