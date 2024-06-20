005a0e801153ffc836617a9533695d7979468661
Kramik <kramikkafle7@gmail.com>
2024-06-20 16:11:44 +0545

    first commit

--- link_list.js
+++ link_list.js
@@ -16,8 +16,18 @@ class List{
         this.tail.next = newNode;
         this.tail = newNode
     }
+    traversing(){
+        let counter = 0;
+        let currentNode = this.head;
+        while(counter<this.size){
+            console.log(currentNode)
+            currentNode = currentNode.next;
+            counter++;
+        }
+    }
 }
 let list = new List(200);
 list.appendNode(300);
 list.appendNode(400);
 console.log(list);
+list.traversing();
