80b8be6f48d97e8fd6dcce4c318b681cf376ffdc
Kramik <kramikkafle7@gmail.com>
2024-06-25 18:05:22 +0545

    first commit

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -49,12 +49,38 @@ class BSTree {
             return this.search(root.right, val);
         }
     }
+    preOrder(root){
+        if(root){
+            console.log(root.value);
+            this.preOrder(root.left);
+            this.preOrder(root.right);
+        }
+    }
+    inOrder(root){
+        if(root){
+            this.inOrder(root.left);
+            console.log(root.value);
+            this.inOrder(root.right);
+        }
+    }
+    postOrder(root){
+        if(root){
+            this.postOrder(root.left);
+            this.postOrder(root.right);
+            console.log(root.value);
+        }
+    }    
 }
 let bst = new BSTree();
 bst.makeTree(20);
 bst.makeTree(3);
 bst.makeTree(21);
 bst.makeTree(30);
+
+bst.preOrder(bst.root);
+bst.inOrder(bst.root);
+bst.postOrder(bst.root);
+
 console.log(bst.search(bst.root, 30));
 
 console.log(bst);
