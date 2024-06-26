d842db00805a0751ebbe4269a6ef3752668a9c67
Kramik <kramikkafle7@gmail.com>
2024-06-26 12:42:24 +0545

    first commit

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -98,12 +98,38 @@ class BSTree {
             return this.minValue(root.right);
         }
     }  
+
+    removeData(value){
+        this.root = this.deleteNode(this.root, value)
+    }
+    deleteNode(root, value){
+        if(root === null){
+            return null;
+        }
+        if(value < this.root.value){
+            root.left = this.deleteNode(root.left, value)
+        } else if(value > this.root.value){
+            root.right = this.deleteNode(root.right, value)
+        } else {
+            if(!root.left && !root.right){
+                return null;
+            } else if(!this.root.left){
+                return this.root.right;
+            } else if(!this.root.right){
+                return this.root.left;
+            }
+            root.value = this.minValue(root.right);
+            root.right = this.deleteNode(root.right, root.value)
+        }
+        return root;
+    }
 }
 let bst = new BSTree();
 bst.makeTree(20);
 bst.makeTree(3);
 bst.makeTree(21);
 bst.makeTree(30);
+bst.makeTree(5); 
 
 // bst.preOrder(bst.root);
 // bst.inOrder(bst.root);
@@ -111,9 +137,11 @@ bst.makeTree(30);
 
 // console.log(bst.search(bst.root, 30));
 
-// bst.bfsTraversing();
 
-console.log(bst.minValue(bst.root));
-console.log(bst.maxValue(bst.root));
+// console.log(bst.minValue(bst.root));
+// console.log(bst.maxValue(bst.root));
+
+bst.removeData(5);
+bst.bfsTraversing(bst.root);
 
 console.log(bst.root);
